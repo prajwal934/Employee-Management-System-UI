38a1bf1d621447ad47771ce67e41d7a4ad48327c
prajwal934 <samalprajwal.302@gmail.com>
2024-11-20 19:41:24 +0530

    added requriments

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,3 @@
-// import React, { useContext, useEffect, useState } from 'react'
-// import Login from './components/Auth/Login'
-// import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-// import AdminDashboard from './components/Dashboard/AdminDashboard'
-// import { AuthContext } from './context/AuthProvider'
-
-// const App = () => {
-
-//   const [user, setUser] = useState(null)
-//   const [loggedInUserData, setLoggedInUserData] = useState(null)
-//   const [userData,SetUserData] = useContext(AuthContext)
-
-//   useEffect(()=>{
-//     const loggedInUser = localStorage.getItem('loggedInUser')
-    
-//     if(loggedInUser){
-//       const userData = JSON.parse(loggedInUser)
-//       setUser(userData.role)
-//       setLoggedInUserData(userData.data)
-//     }
-
-//   },[])
-
-
-//   const handleLogin = (email, password) => {
-//     if (email == 'admin@me.com' && password == '123') {
-//       setUser('admin')
-//       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
-//     } else if (userData) {
-//       const employee = userData.find((e) => email == e.email && e.password == password)
-//       if (employee) {
-//         setUser('employee')
-//         setLoggedInUserData(employee)
-//         localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
-//       }
-//     }
-//     else {
-//       alert("Invalid Credentials")
-//     }
-//   }
-
-
-
-//   return (
-//     <>
-//       {!user ? <Login handleLogin={handleLogin} /> : ''}
-//       {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
-//     </>
-//   )
-// }
-
-// export default App
 
 import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
@@ -58,6 +6,12 @@ import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
+
+  // Logging environment variables for debugging
+  console.log('Environment Variables:', import.meta.env);
+  console.log('App Name:', import.meta.env.VITE_APP_NAME);        
+  console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);   
+  console.log('Feature Flag:', import.meta.env.VITE_FEATURE_FLAG);
 
   const [user, setUser] = useState(null)
   const [loggedInUserData, setLoggedInUserData] = useState(null)
@@ -76,7 +30,7 @@ const App = () => {
 
 
   const handleLogin = (email, password) => {
-    if (email == 'admin@me.com' && password == '123') {
+    if (email == 'prajwal@gmail.com' && password == 'P@123') {
       setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
     } else if (userData) {
